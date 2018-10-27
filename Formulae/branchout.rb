00fff134d2b4aaa3ffe03a9494f30eaaadbbe4b9
Michael McCallum <michael@StickyCode.net>
2018-10-28 00:12:03 +1300

    Staring on a tap for homebrew

---
+++ Formulae/branchout.rb
@@ -0,0 +1,9 @@
+class Branchout < Formula
+
+  desc "Command-line git repository layout manager"
+  homepage "https://stickycode.readthedocs.io"
+  url "https://github.com/StickySource/branchout/blob/v1.2/branchout"
+  sha256 "ec9dcf01f67a2329218fcd090b56042379937b269ddbd8c0c64097636f012e63"
+  revision 1
+
+end
