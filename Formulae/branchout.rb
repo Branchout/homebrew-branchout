38de48228867a99b67e306d8555451e9d0ae032f
Michael McCallum <michael@StickyCode.net>
2018-10-28 22:43:22 +1300

    Branchout v1.3 with a test and instructions to use it

--- Formulae/branchout.rb
+++ Formulae/branchout.rb
@@ -2,8 +2,18 @@ class Branchout < Formula
 
   desc "Command-line git repository layout manager"
   homepage "https://stickycode.readthedocs.io"
-  url "https://github.com/StickySource/branchout/blob/v1.2/branchout"
-  sha256 "ec9dcf01f67a2329218fcd090b56042379937b269ddbd8c0c64097636f012e63"
-  revision 1
-
+  url "https://github.com/StickySource/branchout/archive/v1.3.tar.gz"
+  sha256 "6b648978d62ee012f7f01ff4f7271765794c66288321ca4c7a9b7628f35babc9"
+  version 1.3
+  
+  depends_on "git"
+  depends_on "bash"
+  
+  def install
+    bin.install "branchout"
+  end
+  
+  def test
+    system "#{bin}/branchout" "version"
+  end
 end
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # homebrew-branchout
+
+To install run
+
+```
+brew tap StickySource/branchout
+brew install branchout
+```
