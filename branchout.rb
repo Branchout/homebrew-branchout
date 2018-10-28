4ecc8da6af57e5077ea9e1fed52d71925c53bab2
Michael McCallum <michael@StickyCode.net>
2018-10-29 12:45:15 +1300

    version is a string

--- branchout.rb
+++ branchout.rb
@@ -4,7 +4,7 @@ class Branchout < Formula
   homepage "https://stickycode.readthedocs.io"
   url "https://github.com/StickySource/branchout/archive/v1.3.tar.gz"
   sha256 "6b648978d62ee012f7f01ff4f7271765794c66288321ca4c7a9b7628f35babc9"
-  version 1.3
+  version "1.3"
   
   depends_on "git"
   depends_on "bash"
