8654418fced94ecdb406eaae5cc371e7e703234f
Michael McCallum <michael@StickyCode.net>
2018-10-29 21:50:46 +1300

    version 1.4 with init url support

--- branchout.rb
+++ branchout.rb
@@ -2,9 +2,9 @@ class Branchout < Formula
 
   desc "Command-line git repository layout manager"
   homepage "https://stickycode.readthedocs.io"
-  url "https://github.com/StickySource/branchout/archive/v1.3.tar.gz"
-  sha256 "6b648978d62ee012f7f01ff4f7271765794c66288321ca4c7a9b7628f35babc9"
-  version "1.3"
+  url "https://github.com/StickySource/branchout/archive/v1.4.tar.gz"
+  sha256 "94568fef19e0300016056fd63b8d71b009c8829828e215a884b32e7e682a6217"
+  version "1.4"
   
   depends_on "git"
   depends_on "bash"
