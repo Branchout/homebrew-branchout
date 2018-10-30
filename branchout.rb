16c915af612019586f258f4832ab5cdfa131dba3
Michael McCallum <michael@StickyCode.net>
2018-10-30 20:54:39 +1300

    Grokking brew and github release versions

--- branchout.rb
+++ branchout.rb
@@ -2,9 +2,9 @@ class Branchout < Formula
 
   desc "Command-line git repository layout manager"
   homepage "https://stickycode.readthedocs.io"
-  url "https://github.com/StickySource/branchout/archive/v1.6.tar.gz"
-  sha256 "e5c756a7decca39636015e5d1686139afc8161b66e0392ca730c0f0224d79e46"
-  version "1.6"
+  url "https://github.com/StickySource/branchout/archive/v1.7.tar.gz"
+  sha256 "128b1bfd3bee68e1c5587ce31d06dcc3739f8f7538bbf1904bc13cc767f40037"
+  version "1.7"
   
   depends_on "git"
   depends_on "bash"
