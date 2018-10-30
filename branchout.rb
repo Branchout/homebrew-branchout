7cfe1be5947f35bead9e1a7a1dde7cd1a5a44468
Michael McCallum <michael@StickyCode.net>
2018-10-30 20:39:11 +1300

    Update the url too

--- branchout.rb
+++ branchout.rb
@@ -2,7 +2,7 @@ class Branchout < Formula
 
   desc "Command-line git repository layout manager"
   homepage "https://stickycode.readthedocs.io"
-  url "https://github.com/StickySource/branchout/archive/v1.4.tar.gz"
+  url "https://github.com/StickySource/branchout/archive/v1.5.tar.gz"
   sha256 "11097ff3ea07cef55296f570865f467e4b984f62c0fcf8058531c7d3772334c7"
   version "1.5"
   
