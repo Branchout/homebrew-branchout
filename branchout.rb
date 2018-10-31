eb3eadf91b0ad17c2c9402f8903f174d00eb7a53
Michael McCallum <michael@StickyCode.net>
2018-10-31 15:09:06 +1300

    1.9 with a fix for http init urls

--- branchout.rb
+++ branchout.rb
@@ -1,10 +1,10 @@
 class Branchout < Formula
 
   desc "Command-line git repository layout manager"
-  homepage "https://stickycode.readthedocs.io"
-  url "https://github.com/StickySource/branchout/archive/v1.8.tar.gz"
-  sha256 "b9be1fb2d7de17e2bc2778d0d14d1d197abfc500a9ff2d8da0b50f7bc2e152ea"
-  version "1.8"
+  homepage "https://github.com/Branchout/branchout"
+  url "https://github.com/StickySource/branchout/archive/v1.9.tar.gz"
+  sha256 "5b58da7af87b0e1555cd2abc7a80ef454e3def7ab78e285ba356fa628439a759"
+  version "1.9"
   
   depends_on "git"
   depends_on "bash"
