bcaaf780da6dfb956b758b50dc347839d2d6f689
Michael McCallum <michael@StickyCode.net>
2018-10-31 13:21:45 +1300

    Release 1.8
    * support for legacy .projects and .branchout
    * full test coverage with osx and linux

--- branchout.rb
+++ branchout.rb
@@ -2,9 +2,9 @@ class Branchout < Formula
 
   desc "Command-line git repository layout manager"
   homepage "https://stickycode.readthedocs.io"
-  url "https://github.com/StickySource/branchout/archive/v1.7.tar.gz"
-  sha256 "128b1bfd3bee68e1c5587ce31d06dcc3739f8f7538bbf1904bc13cc767f40037"
-  version "1.7"
+  url "https://github.com/StickySource/branchout/archive/v1.8.tar.gz"
+  sha256 "1b9be1fb2d7de17e2bc2778d0d14d1d197abfc500a9ff2d8da0b50f7bc2e152e"
+  version "1.8"
   
   depends_on "git"
   depends_on "bash"
