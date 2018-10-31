97b982418874d73f69f6a57f3ef146659c59a805
Michael McCallum <michael@StickyCode.net>
2018-10-31 13:23:53 +1300

    wrong hash

--- branchout.rb
+++ branchout.rb
@@ -3,7 +3,7 @@ class Branchout < Formula
   desc "Command-line git repository layout manager"
   homepage "https://stickycode.readthedocs.io"
   url "https://github.com/StickySource/branchout/archive/v1.8.tar.gz"
-  sha256 "1b9be1fb2d7de17e2bc2778d0d14d1d197abfc500a9ff2d8da0b50f7bc2e152e"
+  sha256 "b9be1fb2d7de17e2bc2778d0d14d1d197abfc500a9ff2d8da0b50f7bc2e152ea"
   version "1.8"
   
   depends_on "git"
