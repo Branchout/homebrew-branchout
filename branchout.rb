1fc1674931867bbba8a06e8bf2ce303e9f0d6550
Michael McCallum <michael@StickyCode.net>
2018-11-22 22:55:47 +1300

    update revision for installing branchout maven

--- branchout.rb
+++ branchout.rb
@@ -5,6 +5,8 @@ class Branchout < Formula
   url "https://github.com/StickySource/branchout/archive/v1.13.tar.gz"
   sha256 "01b276596c11b974a28f7a8d56dde5b8cee59f20b9975c883ac5cd5bda08a370"
   version "1.13"
+
+  revision "1"
   
   depends_on "git"
   depends_on "bash"
