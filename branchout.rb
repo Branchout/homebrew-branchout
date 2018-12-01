94705bcb3ccf203209b695a599637b965d9e6a2b
Michael McCallum <michael@stickycode.net>
2018-12-01 23:34:17 +1300

    Release 2.1
    https://github.com/Branchout/branchout/releases/tag/v2.1

--- branchout.rb
+++ branchout.rb
@@ -2,15 +2,20 @@ class Branchout < Formula
 
   desc "Command-line git repository layout manager"
   homepage "https://github.com/Branchout/branchout"
-  url "https://github.com/StickySource/branchout/archive/v1.13.tar.gz"
-  sha256 "01b276596c11b974a28f7a8d56dde5b8cee59f20b9975c883ac5cd5bda08a370"
-  version "1.13.1"
+  url "https://github.com/StickySource/branchout/archive/v2.1.tar.gz"
+  sha256 "bcf05a591b7e1c39adac67cdef777f481f978571cc01ca5fdc490fd12f9f72de"
+  version "2.1"
 
   depends_on "git"
   depends_on "bash"
   
   def install
     bin.install "branchout"
+    bin.install "branchout-project"
+    bin.install "branchout-group"
+    bin.install "branchout-init"
+    bin.install "branchout-environment"
+    bin.install "branchout-configuration"
     bin.install "branchout-maven"
   end
   
