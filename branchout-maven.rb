60c6b20fdf531dfc6f8fa5a64b2abddd3de36a9f
Michael McCallum <michael@stickycode.net>
2019-08-21 10:23:09 +1200

    Bump version to update for users

--- branchout-maven.rb
+++ branchout-maven.rb
@@ -4,7 +4,7 @@ class BranchoutMaven < Formula
   homepage "https://github.com/Branchout/branchout"
   url "https://github.com/StickySource/branchout/archive/v3.1.tar.gz"
   sha256 "87ecbcbd06c7b8d87f1a614b289a641612a418c131230cfc01007f45e63e552e"
-  version "3.1"
+  version "3.1.1"
 
   depends_on "branchout/branchout/core"
   depends_on "maven"
