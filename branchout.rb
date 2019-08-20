c2f3219f67873eee4f4b4714db92a696c99227a2
Michael McCallum <michael@stickycode.net>
2019-08-21 10:43:22 +1200

    Just testing that its all installed

--- branchout.rb
+++ branchout.rb
@@ -4,16 +4,16 @@ class Branchout < Formula
   homepage "https://github.com/Branchout/branchout"
   url "https://github.com/StickySource/branchout/archive/v3.1.tar.gz"
   sha256 "87ecbcbd06c7b8d87f1a614b289a641612a418c131230cfc01007f45e63e552e"
-  version "3.1.2"
+  version "3.1.3"
 
   depends_on "branchout/branchout/branchout-core"
   depends_on "branchout/branchout/branchout-maven"
   depends_on "branchout/branchout/branchout-secrets"
   def install
-    system "#{bin}/branchout version"
+    system "branchout version"
   end
   def test
-    system "#{bin}/branchout version"
+    system "branchout version"
   end
 end
 
