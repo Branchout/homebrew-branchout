5ed01aff77a7470a932bd3856a90484c5947abb9
Michael McCallum <michael@stickycode.net>
2019-08-21 10:41:47 +1200

    Do a noop install to avoid an error

--- branchout.rb
+++ branchout.rb
@@ -4,12 +4,14 @@ class Branchout < Formula
   homepage "https://github.com/Branchout/branchout"
   url "https://github.com/StickySource/branchout/archive/v3.1.tar.gz"
   sha256 "87ecbcbd06c7b8d87f1a614b289a641612a418c131230cfc01007f45e63e552e"
-  version "3.1.1"
+  version "3.1.2"
 
   depends_on "branchout/branchout/branchout-core"
   depends_on "branchout/branchout/branchout-maven"
   depends_on "branchout/branchout/branchout-secrets"
-  
+  def install
+    system "#{bin}/branchout version"
+  end
   def test
     system "#{bin}/branchout version"
   end
