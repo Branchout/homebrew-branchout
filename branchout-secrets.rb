8880f6477038aa926f4d081d78681b8e708338d0
Michael McCallum <michael@stickycode.net>
2019-08-21 10:30:21 +1200

    Typo

--- branchout-secrets.rb
+++ branchout-secrets.rb
@@ -4,9 +4,9 @@ class BranchoutSecrets < Formula
   homepage "https://github.com/Branchout/branchout"
   url "https://github.com/StickySource/branchout/archive/v3.1.tar.gz"
   sha256 "87ecbcbd06c7b8d87f1a614b289a641612a418c131230cfc01007f45e63e552e"
-  version "3.1.1"
+  version "3.1.2"
 
-  depends_on "branchout/branchout/branchout/core"
+  depends_on "branchout/branchout/branchout-core"
   depends_on "stickycode/kubesec/kubesec"
   depends_on "gpg"
   
