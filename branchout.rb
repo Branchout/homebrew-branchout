ca09be983e8baa16743c99b4c7da90391f44dde4
Michael McCallum <michael@stickycode.net>
2020-10-04 22:14:51 +1300

    v4.5

--- branchout.rb
+++ branchout.rb
@@ -2,9 +2,9 @@ class Branchout < Formula
 
   desc "Command-line git repository layout manage"
   homepage "https://github.com/Branchout/branchout"
-  url "https://github.com/StickySource/branchout/archive/v4.4.tar.gz"
-  sha256 "6db9ed711bb4e247b69efc8caccb9bfe1bdee208269a2fc781d221937e519842"
-  version "4.4"
+  url "https://github.com/StickySource/branchout/archive/v4.5.tar.gz"
+  sha256 "ecbdb580e805cf42c12e995f62b84198da1a8352fa8b5ecb0add08204a198c3c"
+  version "4.5"
 
   depends_on "branchout/branchout/branchout-core"
   depends_on "branchout/branchout/branchout-maven"
