6171ff0412a3ac25defbdb42c71e39637479abcc
Michael McCallum <michael@stickycode.net>
2021-06-10 16:01:43 +1200

    v4.8

--- branchout-core.rb
+++ branchout-core.rb
@@ -2,9 +2,9 @@ class BranchoutCore < Formula
 
   desc "Command-line git repository layout manage"
   homepage "https://github.com/Branchout/branchout"
-  url "https://github.com/StickySource/branchout/archive/v4.7.tar.gz"
-  sha256 "2b341e6feb776914351e80463b98ee0370fcaeef894bf77e6c5bf23965fd9553"
-  version "4.7"
+  url "https://github.com/StickySource/branchout/archive/v4.8.tar.gz"
+  sha256 "530470cfd9b8caa056f2d69eae7cfa1f89cad4711c394bff92cd19d7f098da4d"
+  version "4.8"
 
   depends_on "git"
   depends_on "bash"
