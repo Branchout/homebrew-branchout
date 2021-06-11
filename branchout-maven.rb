99052d9b076c4d0f036285b4310380b42e39192e
Michael McCallum <michael@stickycode.net>
2021-06-11 14:25:46 +1200

    v4.9

--- branchout-maven.rb
+++ branchout-maven.rb
@@ -2,9 +2,9 @@ class BranchoutMaven < Formula
 
   desc "Command-line git repository layout manager"
   homepage "https://github.com/Branchout/branchout"
-  url "https://github.com/StickySource/branchout/archive/v4.8.tar.gz"
-  sha256 "530470cfd9b8caa056f2d69eae7cfa1f89cad4711c394bff92cd19d7f098da4d"
-  version "4.8"
+  url "https://github.com/StickySource/branchout/archive/v4.9.tar.gz"
+  sha256 "5e18c1dc7dc05aa3a8e47f40a2e0191e793c6b05ef762c74cc308300527cb773"
+  version "4.9"
 
   depends_on "branchout/branchout/branchout-core"
   depends_on "maven"
