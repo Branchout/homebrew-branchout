e72fe980bf97f9cb85cf5aee8522bff2ef0897ad
Michael McCallum <michael@stickycode.net>
2021-06-18 18:08:09 +1200

    v4.11

--- branchout-yarn.rb
+++ branchout-yarn.rb
@@ -2,9 +2,9 @@ class BranchoutYarn < Formula
 
   desc "Command-line git repository layout manager"
   homepage "https://github.com/Branchout/branchout"
-  url "https://github.com/StickySource/branchout/archive/v4.10.tar.gz"
-  sha256 "328b516b5d3f4b717be24c0996119d7ed51f4b67181ce1a99c6358af6e83098c"
-  version "4.10"
+  url "https://github.com/StickySource/branchout/archive/v4.11.tar.gz"
+  sha256 "1a42ecd7662148b842430bf332ff6da8bf65064d187b937f7cab6e4bdb631a8a"
+  version "4.11"
 
   depends_on "branchout/branchout/branchout-core"
   depends_on "yarn"
