a4b4227abf68576ccf564a83a110a450518edaf0
Michael McCallum <michael@stickycode.net>
2021-09-30 16:45:40 +1300

    v4.13

--- branchout-core.rb
+++ branchout-core.rb
@@ -2,13 +2,13 @@ class BranchoutCore < Formula
 
   desc "Command-line git repository layout manage"
   homepage "https://github.com/Branchout/branchout"
-  url "https://github.com/StickySource/branchout/archive/v4.12.tar.gz"
-  sha256 "f4e9471192d20f527ee4f071cd952bedd421fee29b24a31735d9fb0de423abb6"
-  version "4.12"
+  url "https://github.com/StickySource/branchout/archive/v4.13.tar.gz"
+  sha256 "85b4561a21f72b098cdd94f01cb29cba24757d0dd8812a1a6ba6668b79d813e4"
+  version "4.13"
 
   depends_on "git"
   depends_on "bash"
-  
+
   def install
     bin.install "branchout"
     bin.install "branchout-project"
@@ -17,7 +17,7 @@ class BranchoutCore < Formula
     bin.install "branchout-environment"
     bin.install "branchout-configuration"
   end
-  
+
   def test
     system "#{bin}/branchout version"
   end
--- branchout-maven.rb
+++ branchout-maven.rb
@@ -2,17 +2,17 @@ class BranchoutMaven < Formula
 
   desc "Command-line git repository layout manager"
   homepage "https://github.com/Branchout/branchout"
-  url "https://github.com/StickySource/branchout/archive/v4.12.tar.gz"
-  sha256 "f4e9471192d20f527ee4f071cd952bedd421fee29b24a31735d9fb0de423abb6"
-  version "4.12"
+  url "https://github.com/StickySource/branchout/archive/v4.13.tar.gz"
+  sha256 "85b4561a21f72b098cdd94f01cb29cba24757d0dd8812a1a6ba6668b79d813e4"
+  version "4.13"
 
   depends_on "branchout/branchout/branchout-core"
   depends_on "maven"
-  
+
   def install
     bin.install "branchout-maven"
   end
-  
+
   def test
     system "#{bin}/branchout version"
   end
