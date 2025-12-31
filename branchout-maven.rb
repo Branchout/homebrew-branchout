30bc6eff5a7962d9369c2a35280b4bced0d15d37
Michael McCallum <michael@stickycode.net>
2025-12-31 16:57:13 +1300

    v4.18

--- branchout-maven.rb
+++ branchout-maven.rb
@@ -2,12 +2,12 @@ class BranchoutMaven < Formula
 
   desc "Command-line git repository layout manager"
   homepage "https://github.com/Branchout/branchout"
-  url "https://github.com/StickySource/branchout/archive/v4.16.tar.gz"
-  sha256 "8e2214bd49129128e96ec502a691260327f4979db055e062f63bbc13bf3ce068"
-  version "4.16"
+  url "https://github.com/StickySource/branchout/archive/v4.18.tar.gz"
+  sha256 "cf767c2f67d7d3fc594e1be44556c286345ca10e9161295c19eb25631b785d8f"
+  version "4.18"
 
   depends_on "branchout/branchout/branchout-core"
-  depends_on "maven"
+  depends_on "branchout/branchout/maven@3.8.1"
 
   def install
     bin.install "branchout-maven"
--- branchout-yarn.rb
+++ branchout-yarn.rb
@@ -2,9 +2,9 @@ class BranchoutYarn < Formula
 
   desc "Command-line git repository layout manager"
   homepage "https://github.com/Branchout/branchout"
-  url "https://github.com/StickySource/branchout/archive/v4.16.tar.gz"
-  sha256 "8e2214bd49129128e96ec502a691260327f4979db055e062f63bbc13bf3ce068"
-  version "4.16"
+  url "https://github.com/StickySource/branchout/archive/v4.18.tar.gz"
+  sha256 "cf767c2f67d7d3fc594e1be44556c286345ca10e9161295c19eb25631b785d8f"
+  version "4.18"
 
   depends_on "branchout/branchout/branchout-core"
   depends_on "yarn"
